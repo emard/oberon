f61c5da4530d3e85dc507b49cd68245719ea6b46
emard <vordah@gmail.com>
2019-04-25 14:07:00 +0200

    vendor-independent BRAM (untested). Currently its verilog source throws error when compiling with lattice diamond 3.7

--- hdl/bram32bit.v
+++ hdl/bram32bit.v
@@ -6,12 +6,12 @@ module bram32bit
 )
 (
   input clk_a, clk_b,
+  input clken_a, clken_b,
   input [(addr_width-1):0] addr_a, addr_b,
   input [3:0] we_a, we_b,
   input [31:0] data_in_a, data_in_b,
   output [31:0] data_out_a, data_out_b
 );
-  // parameter initial_file[3:0] = { "init0.mem", "init1.mem", "init2.mem", "init3.meem" };
   generate
     genvar i;
     for(i = 0; i < 3; i++)
@@ -22,14 +22,17 @@ module bram32bit
         .data_width(8),
         .addr_width(addr_width),
         .initial_file("cache_init"+i+".mem")
-        //.initial_file(initial_file[i])
       )
       bram_true2p_2clk_inst
       (
         .clk_a(clk_a),
         .clk_b(clk_b),
+        .clken_a(clken_a),
+        .clken_b(clken_b),
         .we_a(we_a[i]),
         .we_b(we_b[i]),
+        .addr_a(addr_a),
+        .addr_b(addr_b),
         .data_in_a(data_in_a[i*8+7:i*8]),
         .data_in_b(data_in_b[i*8+7:i*8]),
         .data_out_a(data_out_a[i*8+7:i*8]),
--- hdl/bram_true2p_2clk.v
+++ hdl/bram_true2p_2clk.v
@@ -13,21 +13,23 @@ module bram_true2p_2clk
 (
   input [(data_width-1):0] data_in_a, data_in_b,
   input [(addr_width-1):0] addr_a, addr_b,
-  input we_a, we_b, clk_a, clk_b,
+  input we_a, we_b, clk_a, clk_b, clken_a, clken_b,
   output reg [(data_width-1):0] data_out_a, data_out_b
 );
   // Declare the RAM variable
   reg [data_width-1:0] ram[2**addr_width-1:0];
+  initial $readmemh(initial_file);
 
   always @ (posedge clk_a)
   begin
-    // Port A 
-    if (we_a) 
+    // Port A
+    if(clken_a)
+    if(we_a)
     begin
       ram[addr_a] <= data_in_a;
       data_out_a <= data_in_a;
     end
-    else 
+    else
     begin
       data_out_a <= ram[addr_a];
     end
@@ -37,17 +39,18 @@ module bram_true2p_2clk
   if(dual_port)
   always @(posedge clk_b)
   begin
-    // Port B 
-    if (we_b) 
+    // Port B
+    if(clken_b)
+    if(we_b)
     begin
       ram[addr_b] <= data_in_b;
       data_out_b <= data_in_b;
     end
-    else 
+    else
     begin
       data_out_b <= ram[addr_b];
-    end 
+    end
   end
   endgenerate
-  
+
 endmodule
