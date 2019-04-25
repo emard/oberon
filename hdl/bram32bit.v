9e265ef365b54afbf7aa1b26ee8566a6735230a4
emard <vordah@gmail.com>
2019-04-25 12:01:27 +0200

    starting attempt for vendor-independent replacement for BRAM for the cache

---
+++ hdl/bram32bit.v
@@ -0,0 +1,40 @@
+// byte addressable BRAM with initialized content
+
+module bram32bit
+#(
+  parameter addr_width = 12
+)
+(
+  input clk_a, clk_b,
+  input [(addr_width-1):0] addr_a, addr_b,
+  input [3:0] we_a, we_b,
+  input [31:0] data_in_a, data_in_b,
+  output [31:0] data_out_a, data_out_b
+);
+  // parameter initial_file[3:0] = { "init0.mem", "init1.mem", "init2.mem", "init3.meem" };
+  generate
+    genvar i;
+    for(i = 0; i < 3; i++)
+    begin
+      bram_true2p_2clk
+      #(
+        .dual_port(1),
+        .data_width(8),
+        .addr_width(addr_width),
+        .initial_file("cache_init"+i+".mem")
+        //.initial_file(initial_file[i])
+      )
+      bram_true2p_2clk_inst
+      (
+        .clk_a(clk_a),
+        .clk_b(clk_b),
+        .we_a(we_a[i]),
+        .we_b(we_b[i]),
+        .data_in_a(data_in_a[i*8+7:i*8]),
+        .data_in_b(data_in_b[i*8+7:i*8]),
+        .data_out_a(data_out_a[i*8+7:i*8]),
+        .data_out_b(data_out_b[i*8+7:i*8])
+      );
+    end
+  endgenerate
+endmodule
---
+++ hdl/bram_true2p_2clk.v
@@ -0,0 +1,53 @@
+// Quartus Prime Verilog Template
+// True Dual Port RAM with dual clocks
+// File->New File->VHDL File
+// Edit->Insert Template->VHDL->Full designs->RAMs and ROMs->True dual port RAM (dual clock)
+
+module bram_true2p_2clk
+#(
+  parameter dual_port = 0,
+  parameter data_width = 8,
+  parameter addr_width = 6,
+  parameter initial_file = "initial.mem"
+)
+(
+  input [(data_width-1):0] data_in_a, data_in_b,
+  input [(addr_width-1):0] addr_a, addr_b,
+  input we_a, we_b, clk_a, clk_b,
+  output reg [(data_width-1):0] data_out_a, data_out_b
+);
+  // Declare the RAM variable
+  reg [data_width-1:0] ram[2**addr_width-1:0];
+
+  always @ (posedge clk_a)
+  begin
+    // Port A 
+    if (we_a) 
+    begin
+      ram[addr_a] <= data_in_a;
+      data_out_a <= data_in_a;
+    end
+    else 
+    begin
+      data_out_a <= ram[addr_a];
+    end
+  end
+
+  generate
+  if(dual_port)
+  always @(posedge clk_b)
+  begin
+    // Port B 
+    if (we_b) 
+    begin
+      ram[addr_b] <= data_in_b;
+      data_out_b <= data_in_b;
+    end
+    else 
+    begin
+      data_out_b <= ram[addr_b];
+    end 
+  end
+  endgenerate
+  
+endmodule
